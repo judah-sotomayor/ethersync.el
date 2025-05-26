01e509e89d05bb17db7c9204dbd91b2f2dc42f16
Judah Sotomayor <judah.sotomayor@pm.me>
2025-05-26 17:13:15 -0400

    feat: Initialize repository with flake

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    ethersync.url = "github:ethersync/ethersync";
+  };
+
+  outputs = {self, ethersync, nixpkgs} :
+    let
+      forAllSystems = function:
+        nixpkgs.lib.genAttrs [
+          "x86_64-linux"
+          "aarch64-darwin"
+        ] (system: function nixpkgs.legacyPackages.${system} system);
+    in
+      {
+        devShells = forAllSystems (pkgs: system: {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              emacs
+              ethersync.packages.${system}.default
+              git
+            ];
+          };
+        });
+      };
+}
